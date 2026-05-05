3814fb128ef3e4d02375db2def01908155b6f0f6
mirikim404 <mirikim8989@gmail.com>
2026-05-05 22:37:59 +0900

    refactor: add animal package

---
+++ HunterAnimal/src/animal/StrayAnimal.java
@@ -0,0 +1,20 @@
+package animal;
+public class StrayAnimal extends Animal {
+	private String foundLocation;
+	private String foundDate;
+
+	public StrayAnimal() {
+
+	}
+
+	public StrayAnimal(String name, int age, String residence, String diet, String foundLocation, String foundDate) {
+		super(name, age, residence, diet);
+		this.foundLocation = foundLocation;
+		this.foundDate = foundDate;
+	}
+
+	public void beWary() {
+        System.out.println(getName() + "이/가 경계하고 있습니다.");
+	}
+
+}
