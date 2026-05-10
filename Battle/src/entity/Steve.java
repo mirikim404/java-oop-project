9526771e20032503d2e3602669f5a4376187d4af
sammykim67 <sammykim@ewhain.net>
2026-05-11 04:51:01 +0900

    feat: implement Entity and Steve basic structure
    - Entity 생성자 및 maxHealth 초기화 로직 추가
    - Entity getter/setter, takeDamage(int), isAlive(), heal(), block() 기본 구현
    - Steve 생성자 및 exp/level/coin/weapon/skill/consumable 기본 필드 구현
    - Steve attack, gainExp, levelUp, gainCoin, onTurnEnd, resetAfterDeath 기본 로직 구현
    - A파트 기본 동작 확인용 SteveTest 추가

--- Battle/src/entity/Steve.java
+++ Battle/src/entity/Steve.java
@@ -1,35 +1,203 @@
 package entity;
 
+import interfaces.Skillable;
+import skill.active.ActiveSkill;
+import skill.consumable.ConsumableSkill;
+import weapon.Weapon;
+import weapon.WoodSword;
+import entity.mob.Mob;
+
 public class Steve extends Entity implements Skillable {
     private int coin;
     private String gameMode;
     private int level;
     private int exp;
-    private int maxHealth;
-    private int health;
     private Weapon weapon;
     private ActiveSkill[] activeSkills;
     private ConsumableSkill[] consumables;
+    
+    // TODO: 기본 스탯 상수값 설정 - 상의 후 변경가능
+    private static final int DEFAULT_MAX_HEALTH = 100;
+    private static final int DEFAULT_ATTACK_POWER = 10;
+    private static final int DEFAULT_DEFENCE_POWER = 3;
+    private static final int BASE_REQUIRED_EXP = 100;
+    
+    public Steve() {}	// 기본 생성자
+    
+    public Steve(String name, String gameMode) {
+        super(name, DEFAULT_MAX_HEALTH, DEFAULT_ATTACK_POWER, DEFAULT_DEFENCE_POWER);
+        this.coin = 0;
+        this.gameMode = gameMode;
+        this.level = 1;
+        this.exp = 0;
+        this.weapon = new WoodSword();
+        this.activeSkills = new ActiveSkill[2];
+        this.consumables = new ConsumableSkill[2];
 
-    public void attack(){
+        System.out.println("Steve 생성 완료: " + name + ", 모드: " + gameMode);
+    }
+     
+
+    @Override
+	public void attack() {
+        System.out.println(getName() + "이/가 기본 공격을 합니다. 공격력: " + getTotalAttackPower());
+
+	}
+    
+    public void attack(Mob target) {
+        int damage = getTotalAttackPower();
+        System.out.println(getName() + "이/가 " + target.getName() + "을/를 공격합니다. 데미지: " + damage);
+        target.takeDamage(damage);
     }
 
-    public void block(){
-    }
+    
+    @Override
+	public void block() {
+		super.block();
+	}
 
-    public void useSkill(){
+    @Override
+	public void useSkill(){
+        System.out.println(getName() + "이/가 스킬을 사용합니다.");
     }
 
     public void gainExp(){
+    	gainExp(0);
+    }
+    
+    public void gainExp(int amount) {
+        if (amount <= 0) {
+            System.out.println("획득한 경험치가 없습니다.");
+            return;
+        }
+
+        exp += amount;
+        System.out.println(getName() + "이/가 경험치 " + amount + "을/를 획득했습니다. 현재 EXP: " + exp);
+
+        while (exp >= getRequiredExp()) {
+            exp -= getRequiredExp();
+            levelUp();
+        }
     }
 
     public void levelUp(){
+        level++;
+        // 스탯 자동 증가 - 테스트 코딩용 
+        //TODO: 추후 공격력/방어력/최대체력 중 선택으로 수정 필요
+        setMaxHealth(getMaxHealth() + 10);
+        setAttackPower(getAttackPower() + 2);
+        setDefencePower(getDefencePower() + 1);
+        
+        setHealth(getMaxHealth());
+
+        System.out.println(getName() + " 레벨업! 현재 레벨: " + level);
+        System.out.println("최대체력 +10, 공격력 +2, 방어력 +1");	// 테스트 코딩용 (TODO: 수정 필요)
     }
 
     public void gainCoin(){
+    	gainCoin(0);
+    }
+    
+    public void gainCoin(int amount) {
+    	if(amount <=0) {
+    		System.out.println("획득한 코인이 없습니다.");
+    		return;
+    	}
+    	
+    	coin+=amount;
+    	System.out.println(getName()+"이/가 코인 "+amount + "개를 획득했습니다. 현재 코인 : "+coin);
     }
 
-    public void onTurnEnd(){
+    public void onTurnEnd() {
+        if (activeSkills != null) {
+            for (ActiveSkill skill : activeSkills) {
+                if (skill != null) {
+                    skill.decrementCooldown();
+                }
+            }
+        }
+
+        System.out.println(getName() + "의 턴이 종료되었습니다.");
+    }
+    
+    public void resetAfterDeath() {
+    	//TODO: 죽은 뒤 소모품, 무기, 게임모드, 활성스킬 리셋여부 상의 필요
+    	
+    	level = 1;
+    	exp = 0;
+    	
+        setMaxHealth(DEFAULT_MAX_HEALTH);
+        setAttackPower(DEFAULT_ATTACK_POWER);
+        setDefencePower(DEFAULT_DEFENCE_POWER);
+        setHealth(getMaxHealth());
+
+        consumables = new ConsumableSkill[2];
+
+        System.out.println(getName() + "이/가 사망했습니다.");
+        System.out.println("레벨과 경험치는 초기화되고, 코인은 유지됩니다. 현재 코인: " + coin);
+    }
+    
+    private int getRequiredExp() {	
+    	//TODO: 논의 및 밸런스 패치 필요
+        return BASE_REQUIRED_EXP * level;
+    }
+    
+    public int getTotalAttackPower() {
+        if (weapon == null) {	// 무기 없는 경우
+            return getAttackPower();
+        }
+
+        return getAttackPower() + weapon.getAttackBonus();
+    }
+    
+    
+    //getter, setter
+    public int getCoin() {
+        return coin;
+    }
+
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+    
+    public String getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(String gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public ActiveSkill[] getActiveSkills() {
+        return activeSkills;
+    }
+
+    public void setActiveSkills(ActiveSkill[] activeSkills) {
+        this.activeSkills = activeSkills;
+    }
+
+    public ConsumableSkill[] getConsumables() {
+        return consumables;
+    }
+
+    public void setConsumables(ConsumableSkill[] consumables) {
+        this.consumables = consumables;
     }
 
 }
---
+++ Battle/src/game/SteveTest.java
@@ -0,0 +1,64 @@
+package game;
+
+import entity.Steve;
+
+public class SteveTest {
+    public static void main(String[] args) {
+        System.out.println("===== A파트 Steve 테스트 시작 =====");
+
+        Steve steve = new Steve("Steve", "Survival");
+
+        System.out.println("[생성 확인]");
+        System.out.println("이름: " + steve.getName());
+        System.out.println("체력: " + steve.getHealth() + "/" + steve.getMaxHealth());
+        System.out.println("공격력: " + steve.getAttackPower());
+        System.out.println("방어력: " + steve.getDefencePower());
+        System.out.println("레벨: " + steve.getLevel());
+        System.out.println("경험치: " + steve.getExp());
+        System.out.println("코인: " + steve.getCoin());
+        System.out.println("총 공격력: " + steve.getTotalAttackPower());
+
+        System.out.println();
+
+        System.out.println("[공격/방어 테스트]");
+        steve.attack();
+        steve.block();
+
+        System.out.println();
+
+        System.out.println("[데미지 테스트]");
+        steve.takeDamage(30);
+        System.out.println("생존 여부: " + steve.isAlive());
+
+        System.out.println();
+
+        System.out.println("[회복 테스트]");
+        steve.heal(10);
+
+        System.out.println();
+
+        System.out.println("[경험치/레벨업 테스트]");
+        steve.gainExp(120);
+
+        System.out.println();
+
+        System.out.println("[코인 테스트]");
+        steve.gainCoin(50);
+
+        System.out.println();
+
+        System.out.println("[턴 종료 테스트]");
+        steve.onTurnEnd();
+
+        System.out.println();
+
+        System.out.println("[사망 후 초기화 테스트]");
+        steve.resetAfterDeath();
+        System.out.println("사망 후 레벨: " + steve.getLevel());
+        System.out.println("사망 후 경험치: " + steve.getExp());
+        System.out.println("사망 후 코인: " + steve.getCoin());
+        System.out.println("사망 후 체력: " + steve.getHealth() + "/" + steve.getMaxHealth());
+
+        System.out.println("===== A파트 Steve 테스트 종료 =====");
+    }
+}
