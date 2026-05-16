ba934b53f3f7f9f033768addfdeda7ce1f1c9fe7
Luuuuucius <biz_mklim@ewha.ac.kr>
2026-05-16 09:44:49 +0900

    feat : 엔더드래곤 구현 및 각 몹 생성자 점검

--- Battle/src/entity/mob/Piglin.java
+++ Battle/src/entity/mob/Piglin.java
@@ -4,29 +4,34 @@ import ability.DoubleAttack;
 import entity.Entity;
 
 public class Piglin extends Mob {
-	private final int DEFAULT_MAX_HEALTH = 50;
-    private final int DEFAULT_ATTACK_POWER = 25;  
-    private final int DEFAULT_DEFENCE_POWER = 40;
-    private final int DROP_EXP = 50; 
-    private final int DROP_COIN = 50;
-	public Piglin () {} // 기본 생성자
-	
-	public Piglin (String name, int DEFAULT_MAX_HEALTH, int DEFAULT_ATTACK_POWER, int DEFAULT_DEFENCE_POWER) { //생성자
-		super(name, DEFAULT_MAX_HEALTH, DEFAULT_ATTACK_POWER, DEFAULT_DEFENCE_POWER);
+	private static final int DEFAULT_MAX_HEALTH = 50;
+    private static final int DEFAULT_ATTACK_POWER = 25;  
+    private static final int DEFAULT_DEFENCE_POWER = 40;
+    private static final int DROP_EXP = 50; 
+    private static final int DROP_COIN = 50;
+	public Piglin () {
+		super("크리퍼", DEFAULT_MAX_HEALTH, DEFAULT_ATTACK_POWER, DEFAULT_DEFENCE_POWER);
 		setAbility(new DoubleAttack());
-	}
+	} // 기본 생성자
+	
+//	public Piglin (String name, int DEFAULT_MAX_HEALTH, int DEFAULT_ATTACK_POWER, int DEFAULT_DEFENCE_POWER) { //생성자
+//		super(name, DEFAULT_MAX_HEALTH, DEFAULT_ATTACK_POWER, DEFAULT_DEFENCE_POWER);
+//		setAbility(new DoubleAttack());
+//	}
 	
 	
 	@Override
 	public void attack(Entity target) {
+		super.attack(target);
+		System.out.println(getName() + "이(가) 외칩니다.  '금 냄새가 난다... 내놔!'");
 		target.takeDamage(getAttackPower());
-		// TODO Auto-generated method stub
+	
 		
 	}
 
 	@Override
 	public void block() {
-		
+		super.block();
 		
 	}
 
